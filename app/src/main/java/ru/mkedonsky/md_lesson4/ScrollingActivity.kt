9975dca0bddd8631c7e0dbf0d27a06a88e4afeed
alexander <Mkedonsky>
2020-09-10 13:45:30 +0300

    Задание сделал 5 задание

---
+++ app/src/main/java/ru/mkedonsky/md_lesson4/ScrollingActivity.kt
@@ -0,0 +1,11 @@
+package ru.mkedonsky.md_lesson4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrollingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling)
+    }
+}
